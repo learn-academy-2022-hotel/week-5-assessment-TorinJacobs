d63819aa53b8cc5bc3d4054d9fd26df3dccb1ad0
TorinJacobs <potours@yahoo.com>
2023-01-17 00:01:42 -0800

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,14 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { it } = require("node:test")
+// const { describe } = require("yargs")
+
+// const { it } = require("node:test")
+// const { describe } = require("yargs")
+
+// const { exportAllDeclaration } = require("@babel/types")
+// const { it } = require("node:test")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -21,7 +30,74 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+// PSEUDOCODE: Write the test then see the test fail
+// TEST: The test takes two arguments -- the function name and a call back function
+describe("codeMode", () => {
+  // The it method takes two arguments--the function description and a call back function
+  it("takes in a string and returns a string with a coded message", () => {
+    // The expect statement contains the function call and matcher for the expected output
+    expect(codeMode(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(codeMode(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(codeMode(secretCodeWord3)).toEqual("3cc3ntr1c")
+  })
+})
+// RED -- GOOD FAIL
+// FAIL  ./code-challenges.test.js
+// codeMode
+//   ✕ takes in a string and returns a string with a coded message
+
+// ● codeMode › takes in a string and returns a string with a coded message
+
+//   ReferenceError: codeMode is not defined
+
+//     22 | describe("codeMode", () => {
+//     23 |   it("takes in a string and returns a string with a coded message", () => {
+//   > 24 |     expect(codeMode(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//        |     ^
+//     25 |     expect(codeMode(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//     26 |     expect(codeMode(secretCodeWord3)).toEqual("3cc3ntr1c")
+//     27 |   })
+
+//     at Object.expect (code-challenges.test.js:24:5)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.181 s
+// Ran all test suites.
+
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE:
+// PROCESS: Create an object called conversion that contains key:value pairs holding the letter/number assignments.
+// Declare a function called codeMode that utilizes .replace to search for a specific value and replace it with a specified value.
+
+const conversion = {
+  a: 4,
+  e: 3,
+  E: 3,
+  i: 1,
+  o: 0
+}
+const codeMode = (str) =>  str.replace(/[Eaeio]/g, (l) => conversion[l])
+// The function takes a string as a parameter 
+// .replace searches through the string for matching values in the regular expression '/[Eaeio]/g', to be replaced with the values indicated by '(l) => conversion[l]'
+// 'g' is a global modifier meaning it will match all instances of the specified regex values found in the string, not just the first instance in the string
+// '(l) => conversion[l]' indicates returning the values found in the conversion object as a parameter to replace the regex values [Eaeio] that .replace will match and replace in the string argument given
+// HELPFUL RESOURCES: https://stackoverflow.com/questions/59185978/create-a-function-that-takes-a-string-as-an-argument-and-returns-a-coded-h4ck3r
+
+// GREEN -- TEST PASSED
+//  PASS  ./code-challenges.test.js
+//   codeMode
+//   ✓ takes in a string and returns a string with a coded message (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.163 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.85s.
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
@@ -34,11 +110,73 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+// PSEUDOCODE: 
+// TEST: Write the test and see it fail
+describe("wordSelector", () => {
+  it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+    expect(wordSelector(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+    expect(wordSelector(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+  })
+})
+// RED -- GOOD FAIL
+//  FAIL  ./code-challenges.test.js
+//   codeMode
+//   ✓ takes in a string and returns a string with a coded message (1 ms)
+// wordSelector
+//   ✕ takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+
+// ● wordSelector › takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+
+//   ReferenceError: wordSelector is not defined
+
+//     104 | describe("wordSelector", () => {
+//     105 |   it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+//   > 106 |     expect(wordSelector(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+//         |     ^
+//     107 |     expect(wordSelector(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+//     108 |   })
+//     109 | })
+
+//     at Object.expect (code-challenges.test.js:106:5)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.177 s, estimated 1 s
+// Ran all test suites.
+
 // b) Create the function that makes the test pass.
 
-// --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
+// PSEUDOCODE: 
+// INPUT: Array of Strings and a single string
+// OUTPUT: Array of words spelled with the letter in the test variable
+// PROCESS:
+// Declare the function wordSelector that takes in an array of strings and a single string
+const wordSelector = (array, letter) => {
+  // Use .filter method to create a new array with the filtered elements of the original array that meet the callback function specifications. The new array is a shallow copy, meaning it copies elements of the original array as outputs into the new array
+  return array.filter((str) =>
+  // The callback function uses the .toLowerCase method to ensure case matching of uppercase letters and the .includes method to search the words in the array for instances of the letter in the test variable
+  str.toLowerCase().includes(letter))
+}
+// HELPFUL RESOURCES: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#:~:text=The%20filter()%20method%20is,included%20in%20the%20new%20array https://developer.mozilla.org/en-US/docs/Glossary/Shallow_copy https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#copying_methods_and_mutating_methods
 
-// a) Create a test with expect statements using the variable provided.
+
+// GREEN -- TEST PASSED
+//  PASS  ./code-challenges.test.js
+//   codeMode
+//   ✓ takes in a string and returns a string with a coded message (1 ms)
+// wordSelector
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.17 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.75s.
+
+
+// --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -49,4 +187,128 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+// TEST:
+describe("fullHouse", () => {
+  it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+    expect(fullHouse(hand1)).toEqual(true)
+    expect(fullHouse(hand2)).toEqual(false)
+    expect(fullHouse(hand3)).toEqual(false)
+    expect(fullHouse(hand4)).toEqual(true)
+  })
+})
+// RED -- GOOD FAIL
+//  FAIL  ./code-challenges.test.js
+//   codeMode
+//   ✓ takes in a string and returns a string with a coded message (1 ms)
+// wordSelector
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+// fullHouse
+//   ✕ takes in an array of 5 numbers and determines whether or not the array is a full house
+
+// ● fullHouse › takes in an array of 5 numbers and determines whether or not the array is a full house
+
+//   ReferenceError: fullHouse is not defined
+
+//     183 | describe("fullHouse", () => {
+//     184 |   it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+//   > 185 |     expect(fullHouse(hand1)).toEqual(true)
+//         |     ^
+//     186 |     expect(fullHouse(hand2)).toEqual(false)
+//     187 |     expect(fullHouse(hand3)).toEqual(false)
+//     188 |     expect(fullHouse(hand4)).toEqual(true)
+
+//     at Object.expect (code-challenges.test.js:185:5)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 2 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.189 s, estimated 1 s
+// Ran all test suites.
+
+
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE:
+// INPUT: Array of numbers
+// OUTPUT: True/False Boolean Statement
+// PROCESS:
+// Declaring the function fullHouse that takes in an array
+// const fullHouse = (arr) => {
+//   // Creating an empty object to store key:value elements (think cardnumber:instances of cardnumber)
+//   const countObj = {}
+//   // Using the for of loop to iterate through the elements of the array
+//   for(const card of arr){
+//     // Utilizing the .hasOwnProperty method to look for each instance of a key in the countObj
+//     if(countObj.hasOwnProperty(card)){
+//       // if there is an instance of a matching key, increment the value by 1
+//       countObj[card]++
+//       // if there is no instance, set the value to 1
+//     } else {
+//       countObj[card]=1
+//     }
+//   }
+//   // declared a function vals that returns the values of the countObj object's key:value pair (think cardnumber instances)
+//   const vals = Object.values(countObj)
+//   // conditional statement evaluating the key:value pair (again, think cardnumber:instances of cardnumber). If the values in the zeroeth index of the array equal 2 and the values of the 1st index equal 3, OR if the values in the 1st index of the array equal 2 and the values in the zeroeth index of the array equal 3
+//   if((vals[0] === 2 && vals[1] === 3) || (vals[1] === 2 && vals[0] === 3)){
+//     // return true
+//     return true
+//   } 
+//     // otherwise, return false
+//   return false
+// }
+
+// GREEN -- TEST PASSED
+// yarn run v1.22.19
+// warning package.json: No license field
+// $ /Users/learnacademy/Desktop/week-5-assessment-TorinJacobs/node_modules/.bin/jest
+//  PASS  ./code-challenges.test.js
+//   codeMode
+//     ✓ takes in a string and returns a string with a coded message (1 ms)
+//   wordSelector
+//     ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+//   fullHouse
+//     ✓ takes in an array of 5 numbers and determines whether or not the array is a full house (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.179 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.92s.
+
+// HELPFUL RESOURCES:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of https://stackoverflow.com/questions/59510349/how-to-recognise-full-house-and-two-pair-in-a-javascript-poker-game https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
+
+
+// REFACTOR:
+const fullHouse = (arr) => {
+  const countObj = {}
+  for(const card of arr){
+    if(countObj.hasOwnProperty(card)){
+      countObj[card]++
+    } else {
+      countObj[card]=1
+    }
+  }
+  const vals = Object.values(countObj)
+  // If the object values amount to 2 and 3 (a full house), then return true
+  if(vals.includes(2) && vals.includes(3)){
+    return true
+  } 
+  return false
+}
+// GREEN -- ALL TESTS PASSED
+//  PASS  ./code-challenges.test.js
+//   codeMode
+//   ✓ takes in a string and returns a string with a coded message (1 ms)
+// wordSelector
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+// fullHouse
+//   ✓ takes in an array of 5 numbers and determines whether or not the array is a full house (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.181 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.95s.
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,45 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: Calling super pulls values from a higher level of a class to a lower level within the class.
 
-Researched answer:
+Researched answer: Calling super retrieves values inherited from the superclass into the subclass. A superclass is a class that shares common behaviors that are more general. Subclasses have more specific data to its class, but will also match data found in the superclass. This inheritance relationship between a superclass and subclass is what calling super is for, pulling data down from the superclass to the subclass, and not the other way around.
 
-2. What is a gem?
+2. What is a gem? 
 
-Your answer:
+Your answer: A gem is a package of features for the Ruby programming language that provides additional functionality.
 
-Researched answer:
+Researched answer: A package of code functionality and features and their associated files for the Ruby programming language. Ruby gems can be shared with other Ruby programmers so they can use the same features/functionality in Ruby. Ruby gems can also be updated or altered. 
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a database that is not only able to perform create, retrieve, update, and delete actions on data within tables and columns, but create relationships between different tables/columns within a database so that larger amounts of data can be more easily managed.
 
-Researched answer:
+Researched answer: A relational database has tables of data stored in rows and columns like other databases, but they are unique in that the data in the tables can have a relationship with data stored in a separate table.
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: The primary key is the unique identifier attached to the data stored in a row of a table in a database. It's important because it provides an identifier to retrieve information even if the location of the data has changed. It also provides cross reference capability for more than one table which is an important feature in relational databases.
 
-Researched answer:
+Researched answer: Primary keys provide unique identifiers to data entered in the row of a table. This is important because it prevents errors and/or confusion when retrieving data that shares the same name, like people's names. The primary key ensures that repeat names are still unique entries in the database.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: I don't recall the exact HTTP verbs, but I know some of them perform the following functions: Edit, Review, Delete. 
 
-Researched answer:
+Researched answer: The HTTP verbs along with their corresponding CRUD actions followed by a colon are--get:read, post:create, put:update, patch:update, and delete:delete. Get displays data; post, creates data; put, replaces old data with new data; patch, replaces data that's changed; and finally, delete, removes data.
+
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A term referring to a pattern to follow to establish the flow of data between the server and the client. REST is an acronym for Representational State Transfer.
 
-2. Model validations:
+2. Model validations: A validation method used to ensure clean code in the database.
 
-3. Params:
+3. Params: URL Parameters, or query strings refers to a controller method receiving data to query a database and/or change the view dynamically.
 
-4. ERB:
+4. ERB: Embedded Ruby or Templated Ruby allows coding in HTML and embedding Ruby code into the view file.
 
-5. API:
+5. API: Application Programming Interface is a software intermediary fostering interaction between separate applications.
